bffd42f5fd658513c5645740de052d6c58c4cecd
Christian Barrios <christianbarrios15@outlook.com>
2024-04-01 11:49:33 -0300

    Ejercicio de prueba 20resp

---
+++ Prácticos/ejemplo2.php
@@ -0,0 +1,8 @@
+<?php 
+    $var1 = $_GET["num1"];
+    $var2 = $_GET["num2"];
+
+    $var3 = $var1 + $var2;
+    echo "El resultado es $var3";
+
+
---
+++ Prácticos/ejercicio20.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET" action="ejercicio20resp.php">
+        <label for="nombre">Nombre:</label><br>
+        <input type="text" id="nombre" name="nombre"><br>
+        <label for="apellido">Apellido:</label><br>
+        <input type="text" id="apellido" name="apellido">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
